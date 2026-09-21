54aee26eaa57f92a1de61bfbc146ca6d7479b1a8
poojaiiit17 <poojaiiit17@gmail.com>
2026-09-21 22:58:54 +0530

    Build IRCTC railway reservation starter project

---
+++ backend/src/main/java/com/irctc/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.irctc.repository;
+
+import com.irctc.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmailAndPassword(String email, String password);
+    boolean existsByEmail(String email);
+}
