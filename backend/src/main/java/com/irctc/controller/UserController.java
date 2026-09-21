1f49e2156277fed41ee2e91e75110376f60856f3
poojaiiit17 <poojaiiit17@gmail.com>
2026-09-21 22:59:13 +0530

    Build IRCTC railway reservation starter project

---
+++ backend/src/main/java/com/irctc/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.irctc.controller;
+
+import com.irctc.model.User;
+import com.irctc.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin(origins = "*")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public User register(@RequestBody User user) {
+        return userService.register(user);
+    }
+
+    @PostMapping("/login")
+    public User login(@RequestBody User user) {
+        return userService.login(user.getEmail(), user.getPassword());
+    }
+}
