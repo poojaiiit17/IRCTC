c906cc0f17917e8f4f1492a21da381731334fefb
poojaiiit17 <poojaiiit17@gmail.com>
2026-09-21 22:59:01 +0530

    Build IRCTC railway reservation starter project

---
+++ backend/src/main/java/com/irctc/repository/BookingRepository.java
@@ -0,0 +1,9 @@
+package com.irctc.repository;
+
+import com.irctc.model.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+    List<Booking> findByUserId(Long userId);
+}
