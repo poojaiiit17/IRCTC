3c570e4f1bd77513865ceac347592e89c275030a
poojaiiit17 <poojaiiit17@gmail.com>
2026-09-21 23:00:18 +0530

    Add dummy payment and database setup

---
+++ backend/src/main/java/com/irctc/repository/PaymentRepository.java
@@ -0,0 +1,7 @@
+package com.irctc.repository;
+
+import com.irctc.model.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+}
